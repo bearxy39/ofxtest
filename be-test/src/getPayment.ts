58c879879450fd9ff0a14227c2d0f9ad899b8439
Yong Xu <bearxy39@hotmail.com>
2025-08-27 21:28:46 +1000

    Implemented getPayment handler and added more unit tests for it

--- be-test/src/getPayment.ts
+++ be-test/src/getPayment.ts
@@ -1,5 +1,23 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { buildResponse } from './lib/apigateway';
+import { getPayment, Payment } from './lib/payments';
+import { validate as isValidUUID } from 'uuid';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    const paymentId = event.pathParameters?.id;
+
+    if (!paymentId) {
+        return buildResponse(400, { error: 'Missing payment id.' });
+    }
+
+    if(!isValidUUID(paymentId)) {
+        return buildResponse(400, { error: 'Invalid payment id' });
+    }
+
+    const payment = await getPayment(paymentId);
+    if (!payment) {
+        return buildResponse(404, { error: 'Payment not found with the payment id' });
+    }
+
+    return buildResponse(200, payment);
 };
